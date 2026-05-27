3e40224ec5d811e959f550600b8d0a37198fc8dc
Chris Zimmerman <zimm@Chriss-MacBook-Air.local>
2026-05-26 22:30:29 -0500

    public market fact

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,7 @@ const WORD_BANK = {
   "2026-05-24": { word: "FIZZY", fact: "As in, the complimentary sparkling water at Colectivo on Monroe St. It's a nice little perk you might not know about. Grab an espresso, cleanse your pallette with some bubbly, and live your most Parisian life right here in Madison."},
   "2026-05-25": { word: "PATIO", fact: "As in, the cool little patio tucked behind the Education Building on Bascom Hill. During the school year, there's also a small cafe inside the building. Grab a lunch, relax while you eat, and take a walk on the Lakeshore path after."},
   "2026-05-26": { word: "UNDER", fact: "As in, free underground parking at Trader Joe's on Monroe St. Don't pay for those tiny little spots up top. Enjoy complimentary lower level parking (enter on W Lawn Ave) and treat yourself to a bottle of Three Buck Chuck with the money you save."}
+  "2026-05-27": { word: "MAPLE", fact: "As in, Wisconsin's state tree, the sugar maple. These trees typically have lifespans of 200-400 years, roughly as long as we've been waiting for the Madison Public Market to open."}
 
 
 };
